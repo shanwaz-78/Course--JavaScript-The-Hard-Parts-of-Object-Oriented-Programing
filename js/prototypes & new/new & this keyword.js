5da587154607165e679451f960da1a60805e3d95
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-05-06 12:41:01 +0530

    learned new topics

---
+++ js/prototypes & new/new & this keyword.js
@@ -0,0 +1,5 @@
+// __proto__ & prototype is totally different to each other the __proto__ is a reference to the prtotype to create specific objects and the prototype is created by defult with the functions or objects.
+
+// new keyword, new keyword create objects and when we invoke it in the function the function will invoke in the new empty object and whatever object are made he will return that object as well as.
+
+// this keyword, this keyword referse to the global this(window object) let's say i made an object and used the this keyword in that case he will refer to the global object and if i put them into a method then he will refer to his own object.
