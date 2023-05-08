c762fc7fafe1f8418b3aba577705b1022042d7e2
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-05-08 10:55:20 +0530

    learned new keyword

--- js/scope & this/calling prototype methods.js
+++ js/scope & this/calling prototype methods.js
@@ -8,6 +8,6 @@ userCreate.prototype.login = function () {
 };
 
 const user1 = new userCreate("Phill", 4);
-user1.login(); // -> 'Login'
+user1.login(); // -> 'Login'.
 
 // we called login() function he will make new execution context in his execution cotext first thing will store this.score++ in the code execution face then he will store store in local memory the object user1 with key of this. and then in code execution he will insert the user1.score++ and then he will in his own execution context he could'nt find then he will be go to the user1 and he will find it there he couldn't able to find there and he will be look at the __proto__ and the __proto__ is the refference to the prototype object then he will look in the prototype object and he able to find score there and then he will increment it and return us.
