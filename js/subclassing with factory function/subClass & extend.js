9768362197daecf3c436da9c66736fd9ceda6a8b
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-05-10 11:16:23 +0530

    subclassing

--- js/subclassing with factory function/subClass & extend.js
+++ js/subclassing with factory function/subClass & extend.js
@@ -20,3 +20,30 @@ class subClass extends parent {
 }
 const user3 = new subClass("Suzzy", 4, 21);
 console.log(user3.age);
+
+// Seprate instance. from all.
+class scStudent {
+  constructor(name) {
+    this._name = name;
+  }
+  implement() {
+    console.log("name is " + scStudent.capitalize(this.name));
+  }
+  static capitalize(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1); // the static runs only once and it runs only once.
+  }
+  get name() {
+    // getters
+    return this._name;
+  }
+  // also we can make getter and setter private.
+  set name(newName) {
+    // setters
+    this._name = newName;
+  }
+}
+
+const user5 = new scStudent("Phill");
+user5.implement();
+user5.name = "Suzy";
+console.log(user5.name);
